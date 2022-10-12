1770b94988eae3c54d5833cd3b32738826021b6d
Soseit Bedoria <soseitbedoria@gmail.com>
2022-10-12 16:53:17 +0800

    Added my main file

--- 2022-10-05-Activity/Main.java
+++ 2022-10-05-Activity/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -6,52 +7,55 @@ public class Main{
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        List<Country> listOfCountry = new ArrayList<Country>();
-
-        System.out.println("=====================================");
-        System.out.println("Welcome to DreamLand!");
-        System.out.println("=====================================");
-        System.out.println("Note: You can only input an integer.");
-        System.out.println("=====================================");
+        List<Siblings> listOfSiblings = new ArrayList<Siblings>();
         
+        System.out.println("=======================");
+        System.out.println("I am excited to know your siblings!");
+        System.out.println("Note: You can only input an integer.");
+        System.out.println("=======================");
+
         try{
-            System.out.print("Number of Country you want to visit: ");
-            int objects  = sc.nextInt();
+            System.out.print("Number of Sibling/s You Have: ");
+            Integer objects = input.nextInt();
             
             if(objects < 0){
                 input.close();
-                sc.close();
-               
+                scan.close();
+                throw new SiblingsException("Your input is negative. Please input positive index.");
             }
 
             for(int i = 0; i < objects; i++){
                 
                 System.out.println("");
-                System.out.println("Dream Country: ");
-                String countryname = input.nextLine();
-    
+                System.out.print("Name: ");
+                String name_s = scan.nextLine();
 
-                listOfCountry.add(new Country(countryname));
+                System.out.print("Age: ");
+                int age_s = Integer.parseInt(scan.nextLine());
+    
+                listOfSiblings.add(new Siblings(name_s, age_s));
             }
 
             System.out.println("");
-            System.out.println("Here's Your Dream Country List!");
-            System.out.println("=====================================");
-            for(Country list : listOfCountry){
-                System.out.println(list.getCountry());    
+            System.out.println("Here is your Siblings!");
+            System.out.println("=======================");
+            for(Siblings list : listOfSiblings){
+                System.out.println("Name: " + list.getName() + " || " + "Age: " + list.getAge());    
             }
-            System.out.println("=====================================");
+            System.out.println("=======================");
             System.out.println("");
 
             input.close();
-            sc.close();
-             throw new CountryException("");
-            
-             
-        }catch(CountryException e){
+            scan.close();
+
+        }
+        catch(SiblingsException e){
             System.out.println(e.getMessage());
+        }
+        catch(InputMismatchException ex){
+            System.out.println("Your input is not a number.");
         }
 
     }

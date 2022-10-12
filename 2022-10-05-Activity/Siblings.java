b9aa85e49a6cb33b855d7730220c66e8b65e5287
Soseit Bedoria <soseitbedoria@gmail.com>
2022-10-12 17:04:02 +0800

    Added another class

---
+++ 2022-10-05-Activity/Siblings.java
@@ -0,0 +1,17 @@
+public class Siblings{
+    private String name;
+    private int age;
+    
+      public Siblings(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+}
